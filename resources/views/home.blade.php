c3deb9e75cbe8e4cbd335d85ffecaf12aa63367d
luizhenrick-programmer <luizhenrick.programmer@gmail.com>
2024-11-19 18:04:25 -0300

    Codigo Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     .image-container img {
         transition: transform 0.3s ease; /* Suaviza o efeito de zoom */
         transform: none; /* Tamanho normal */
-        
+
     }
     .image-container img:hover {
         transform: scale(1.2); /* Aplica o zoom ao passar o mouse */
@@ -29,51 +29,7 @@
 </style>
 
 <x-app-layout>
-    <header class="header d-flex justify-content-around align-items-center bg-white border-bottom shadow-sm py-3 px-4">
-        <div class="d-flex align-items-center">
-            <a href="{{ route('dashboard') }}" class="logo">
-                <img src="{{ asset('assets/images/logoSite.png') }}" alt="Logo Flor Oficial" width="80" height="80">
-            </a>
-            <div>
-                <span class="d-block" style="font-family: 'Roboto', sans-serif; font-weight: bold;">FLOR OFICIAL</span>
-                <span class="d-block" style="font-family: 'Roboto', sans-serif;">BY Thays Conrado</span>
-            </div>
-        </div>
-
-        <nav class="navbar">
-            <div class="container-fluid">
-                <ul class="nav nav-underline">
-                    <li class="nav-item">
-                      <a class="nav-link text-dark" href="{{ route('dashboard') }}">Início</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-dark" href="{{ route('shopping') }}">Comprar</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link text-dark" href="{{ route('about') }}">Sobre</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="{{ route('contact') }}">Contato</a>
-                    </li>
-                  </ul>
-            </div>
-        </nav>
-
-        <div class="d-flex align-items-center">
-            <a href="#" class="me-3">
-                <img src="{{ asset('assets/images/imagesIcons/search_glass.svg') }}" alt="Buscar" width="21" height="24"></a>
-            <a href="{{ route('profile.edit') }}" class="me-3">
-                <img src="{{ asset('assets/images/imagesIcons/user_solid.svg') }}" alt="Perfil do Usuário" width="20" height="24">
-            </a>
-            <a href="#" class="me-3">
-                <img src="{{ asset('assets/images/imagesIcons/heart_solid.svg') }}" alt="Favoritos" width="23" height="24">
-            </a>
-            <a href="#">
-                <img src="{{ asset('assets/images/imagesIcons/cart_shop.svg') }}" alt="Carrinho de Compras" width="22" height="24">
-            </a>
-        </div>
-    </header>
-
+    @include('partials.header')
     <main>
         <div class="container">
             <section id="destaque" class="row w-100 d-flex align-items-center min-vh-100">
@@ -84,7 +40,7 @@
                         <span class="text-pink-500">de Primavera</span>
                     </h1>
                     <a href="#" class="btn btn-link flex align-self-start text-decoration-none mt-3 text-dark fw-bold p-0">COMPRE AGORA</a>
-                
+
                     <!-- Pagination -->
                     <div class="d-flex align-items-center mt-4">
                         <div class="carousel-indicators">
@@ -95,7 +51,7 @@
                     </div>
               </div>
 
-              <!-- Image Section / images Carousel --> 
+              <!-- Image Section / images Carousel -->
               <div class="col-md-6 d-flex justify-content-center image-container">
                 <div id="carouselExampleIndicators" class="carousel slide">
                   <div class="carousel-inner">
@@ -152,43 +108,67 @@
             <section id="ofertas" class="mt-5 row w-100 d-flex d-flex align-items-center justify-content-center">
                 <h2 class="d-flex align-items-center justify-content-center">Para encher seu carrinho</h2>
                 <div class="mt-4 d-block w-25">
-                    <h3 class="text-pink-500">Ofertas Primavera<br>
+                    <h3 class="text-pink-500 text-center">Ofertas Primavera<br>
                         <span class="fw-bold fs-4">até 60% de desconto</span>
                     </h3>
-                    <a href="#" class="mt-5 fw-bold fs-5 text-dark text-center">Ver tudo</a>
+                    <div class="mt-3 fw-bold fs-5 text-dark flex align-items-center justify-content-center">
+                        <a href="#">Ver tudo</a>
+                    </div>
                 </div>
                 <div class="mt-4 d-flex w-75">
                     <div class="d-block me-2">
-                        <img src="https://via.placeholder.com/300x400" alt="Categoria 1">
+                        <img src="{{ asset('assets/images/imagesOferta/oferta-1.png') }}" alt="Categoria 1">
                         <p class="category-title mt-3">Produto 1</p>
                     </div>
                     <div class="d-block me-2">
-                        <img src="https://via.placeholder.com/300x400" alt="Categoria 2">
+                        <img src="{{ asset('assets/images/imagesOferta/oferta-2.png') }}" alt="Categoria 2">
                         <p class="category-title mt-3">Produto 2</p>
                     </div>
                     <div class="d-block me-2">
-                        <img src="https://via.placeholder.com/300x400" alt="Categoria 3">
+                        <img src="{{ asset('assets/images/imagesOferta/oferta-3.png') }}" alt="Categoria 3">
                         <p class="category-title mt-3">Produto 3</p>
                     </div>
                     <div class="d-block me-2">
-                        <img src="https://via.placeholder.com/300x400" alt="Categoria 4">
+                        <img src="{{ asset('assets/images/imagesOferta/oferta-4.png') }}" alt="Categoria 4">
                         <p class="category-title mt-3">Produto 4</p>
                     </div>
                 </div>
             </section>
-            <section>
-                <div id="destaque-promocao" class="bg-body-tertiary rounded w-50 imagem-relative" style="width: 100px; height: 600px;">
+            <section class="mt-5 row w-100 d-flex d-flex align-items-center justify-content-center">
+                <div id="destaque-promocao-1" class="bg-body-tertiary rounded imagem-relative" style="width: 400px; height: 500px; position: relative;">
                     <div class="rounded-circle bg-danger d-flex align-items-center justify-content-center p-3 circulo-absolute">
-                    <p class="text-white ">A partir<br>
-                        <span>de R$ 20</span>
-                    </p>
+                        <p class="text-white text-center">A partir<br>
+                            <span>de R$ 20</span>
+                        </p>
                     </div>
 
-                    <img src="{{ asset('assets/images/categoriaFOREY.png') }}">
+                    <img src="{{ asset('assets/images/homem.jpg') }}">
 
-                    <div>
-                        <h3 class="text-black">Promoção</h3>
-                        <a href="#">Compre Agora</a>
+                    <div class="bg-white position-absolute rounded w-60 p-3 d-flex flex-column align-items-center justify-content-center" style="margin-top: -75px; left: 20%;">
+                        <div class="d-block">
+                            <h3 class="text-black text-center fs-6 fw-bold">Promoção</h3>
+                        </div>
+                        <div class="d-block mx-auto">
+                            <a href="#">Compre Agora</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="destaque-promocao-2" class="bg-body-tertiary rounded imagem-relative" style="width: 400px; height: 500px; position: relative;">
+                    <div class="rounded-circle bg-danger d-flex align-items-center justify-content-center p-3 circulo-absolute">
+                        <p class="text-white text-center">A partir<br>
+                            <span>de R$ 20</span>
+                        </p>
+                    </div>
+
+                    <img src="{{ asset('assets/images/mulher.jpg') }}">
+
+                    <div class="bg-white position-absolute rounded w-60 p-3 d-flex flex-column align-items-center justify-content-center" style="margin-top: -75px; left: 20%;">
+                        <div class="d-block">
+                            <h3 class="text-black text-center fs-6 fw-bold">Promoção</h3>
+                        </div>
+                        <div class="d-block mx-auto">
+                            <a href="#">Compre Agora</a>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -231,55 +211,12 @@
                             <p class="category-title mt-3">Produto 8</p>
                         </div>
                     </div>
-                    <a href="{{ route('shopping') }}" class="d-flex align-items-center justify-content-center">Ver tudo</a>
+                    <div class=" mt-5 d-flex align-items-center justify-content-center">
+                        <a href="{{ route('shopping') }}">Ver tudo</a>
+                    </div>
                 </div>
             </section>
         </div>
     </main>
-    <footer class="bg-body-tertiary text-black py-3 mt-3 mt-lg-5">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-              <img src="{{asset('assets/images/logoSite.png') }}" alt="Logo da Flor de Pitanga" class="mb-3" style="width: 150px; height: 150px;">
-              <p>R. Visc. de Porto Seguro, 339 - Centro, Formosa-GO, 73801-010</p>
-              <p>flordipintanga@gmail.com</p>
-              <p>+55 61 99999-9999</p>
-              <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-white"><i class="fab fa-facebook-f"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-white"><i class="fab fa-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="text-white"><i class="fab fa-envelope"></i></a></li>
-              </ul>
-            </div>
-            <div class="col-md-2">
-              <h5>Compras</h5>
-              <ul class="list-unstyled">
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-              </ul>
-            </div>
-            <div class="col-md-2">
-              <h5>Categorias</h5>
-              <ul class="list-unstyled">
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-              </ul>
-            </div>
-            <div class="col-md-2">
-              <h5>Help</h5>
-              <ul class="list-unstyled">
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-                <li><a href="#" class="">Exemplo</a></li>
-              </ul>
-            </div>
-          </div>
-          <hr>
-          <p class="text-center">&copy; 2024 Flor de Pitanga</p>
-        </div>
-      </footer>
+    @include('partials.footer')
 </x-app-layout>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header class="header d-flex justify-content-around align-items-center bg-white border-bottom shadow-sm py-3 px-4">
+    <div class="d-flex align-items-center">
+        <a href="{{ route('dashboard') }}" class="logo">
+            <img src="{{ asset('assets/images/logoSite.png') }}" alt="Logo Flor Oficial" width="80" height="80">
+        </a>
+        <div>
+            <span class="d-block" style="font-family: 'Roboto', sans-serif; font-weight: bold;">FLOR OFICIAL</span>
+            <span class="d-block" style="font-family: 'Roboto', sans-serif;">BY Thays Conrado</span>
+        </div>
+    </div>
+
+    <nav class="navbar">
+        <div class="container-fluid">
+            <ul class="nav nav-underline">
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="{{ route('dashboard') }}">Início</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="{{ route('shopping') }}">Comprar</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="{{ route('about') }}">Sobre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="{{ route('contact') }}">Contato</a>
+                </li>
+              </ul>
+        </div>
+    </nav>
+
+    <div class="d-flex align-items-center">
+        <a href="#" class="me-3">
+            <img src="{{ asset('assets/images/imagesIcons/search_glass.svg') }}" alt="Buscar" width="21" height="24"></a>
+        <a href="{{ route('profile.edit') }}" class="me-3">
+            <img src="{{ asset('assets/images/imagesIcons/user_solid.svg') }}" alt="Perfil do Usuário" width="20" height="24">
+        </a>
+        <a href="#" class="me-3">
+            <img src="{{ asset('assets/images/imagesIcons/heart_solid.svg') }}" alt="Favoritos" width="23" height="24">
+        </a>
+        <a href="#">
+            <img src="{{ asset('assets/images/imagesIcons/cart_shop.svg') }}" alt="Carrinho de Compras" width="22" height="24">
+        </a>
+    </div>
+</header>
